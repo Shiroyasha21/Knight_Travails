45ee5a3943d905978435f2b745b5fa7c7fafd9d1
Shiroyasha21 <red.ezequiel@gmail.com>
2022-11-09 16:08:49 +0800

    wrong implementation, commit for record

--- lib/knight.rb
+++ lib/knight.rb
@@ -1,7 +1,49 @@
-class Knight
-  attr_accessor :moves
+class ActionNode
+  attr_accessor :x, :y, :value, :child
 
-  def initialize
-    
+  def initialize(coordinate)
+    @x = coordinate[0]
+    @y = coordinate[1]
+    @value = [x,y]
+    @child = []
   end
+
+  def add_child(node)
+    return if node.nil?
+
+    @child << node
+  end
+end
+
+class Knight
+  attr_accessor :start
+
+  def initialize(coordinate)
+    @start = create_action_graph(coordinate)
+  end
+
+  def create_action_graph(coordinate)
+    x = coordinate[0]
+    y = coordinate[1]
+    return nil if x.negative? || y.negative?
+    return nil if x > 7 || y > 7
+    
+    start = ActionNode.new(coordinate)
+
+    start.add_child(create_action_graph([(x + 1), (y + 2)]))
+
+    
+    start
+  end
+end
+
+k = Knight.new([5,5])
+
+puts 'Start'
+p k.start
+puts 'Child'
+p k.start.child
+
+def knight_moves(initial_pos, target)
+  possible_actions = Knight.new(initial_pos)
 end
